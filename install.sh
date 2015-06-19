8629126d8667c13846527daf8ed27a6fe9da7078
dinstein <dinstein@163.com>
2015-06-19 22:33:28 +0800

    add backup

--- install.sh
+++ install.sh
@@ -4,11 +4,14 @@ DOTFILES=(gitconfig tmux.conf pythonrc.py screenrc)
 
 PWD=$(pwd $0)
 
+mkdir -p backup
+
 # Install DOTFILES
 for file in ${DOTFILES[*]}; do
     echo "Install $file"
     CONFIG_FILE="$HOME/.$file"
     if [ -f $CONFIG_FILE ]; then
+        cp $CONFIG_FILE backup/
         rm $CONFIG_FILE
     fi
     ln -s "$PWD"/"$file" "$CONFIG_FILE"
@@ -20,6 +23,7 @@ done
 ZSH_CONFIG="$ZSH/custom/dinstein.zsh"
 echo "Install dinstein.zsh"
 if [ -f $ZSH_CONFIG ];then
+    cp $zsh_CONFIG backup/
     rm $ZSH_CONFIG
 fi
 ln -s "$PWD/dinstein.zsh" "$ZSH_CONFIG"
